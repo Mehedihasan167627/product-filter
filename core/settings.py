8254066a923739236591674782e88299b553dcc3
Mehedi Hasan <mehedinahid27@gmail.com>
2023-05-29 12:13:55 +0600

    modified: html text

--- core/settings.py
+++ core/settings.py
@@ -124,6 +124,7 @@ STATICFILES_DIRS=[
 MEDIA_URL="/download/images/"
 MEDIA_ROOT = BASE_DIR
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
